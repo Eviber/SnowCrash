d1963574389cdd92c5672521632cf96c771d1221
HdrLucie <hlucie@student.42.fr>
2024-08-25 18:39:08 +0200

    [LUCIE] : Fix parameters crash in reverse.go

--- level09/reverse.go
+++ level09/reverse.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+    if len(os.Args) < 2 {
+        fmt.Printf("Enter a parameter\n")
+        return 
+    }
     argv := []byte(os.Args[1])
     for i, c := range argv {
         fmt.Printf("%c", c-byte(i));
